cb12d5cad802b36ab31613cf37091d5c7beed679
Ricardo Alves <ricardo_m_alves@outlook.com>
2025-07-24 19:44:39 +0100

    fix: missing underscore with sendgrid api key

--- Quivi.Infrastructure.Mailing.SendGrid/SendGridEmailService.cs
+++ Quivi.Infrastructure.Mailing.SendGrid/SendGridEmailService.cs
@@ -14,24 +14,45 @@ namespace Quivi.Infrastructure.Mailing.SendGrid
 
         public async Task SendAsync(MailMessage message, IEnumerable<MailAttachment> attachments)
         {
-            var options = new SendGridClientOptions
+            try
             {
-                ApiKey = this.ApiKey
-            };
-            var client = new SendGridClient(options);
-            var msg = new SendGridMessage
-            {
-                From = new EmailAddress(this.FromAddress, this.FromName),
-                Subject = message.Subject,
-                HtmlContent = message.Body,
-            };
-            msg.AddTo(new EmailAddress(message.ToAddress));
-            foreach (var attachment in attachments.Where(a => a.Bytes.Length > 0))
-            {
-                var ms = new MemoryStream(attachment.Bytes);
-                await msg.AddAttachmentAsync(attachment.Name, ms);
+                var options = new SendGridClientOptions
+                {
+                    ApiKey = this.ApiKey
+                };
+                var client = new SendGridClient(options);
+
+                var msg = new SendGridMessage
+                {
+                    From = new EmailAddress(this.FromAddress, this.FromName),
+                    Subject = message.Subject,
+                    HtmlContent = message.Body,
+                };
+
+                msg.AddTo(new EmailAddress(message.ToAddress));
+
+                foreach (var attachment in attachments.Where(a => a.Bytes.Length > 0))
+                {
+                    using var ms = new MemoryStream(attachment.Bytes);
+                    await msg.AddAttachmentAsync(attachment.Name, ms);
+                }
+
+                Console.WriteLine($"Sending email to: {message.ToAddress}");
+                Console.WriteLine($"From: {this.FromAddress}");
+                Console.WriteLine($"Subject: {message.Subject}");
+
+                var response = await client.SendEmailAsync(msg);
+                var responseBody = await response.Body.ReadAsStringAsync();
+
+                Console.WriteLine($"SendGrid response status: {response.StatusCode}");
+                Console.WriteLine($"SendGrid response body: {responseBody}");
             }
-            await client.SendEmailAsync(msg);
+            catch (Exception ex)
+            {
+                // Log to standard error
+                Console.Error.WriteLine($"[SendEmail ERROR] {ex.GetType().Name}: {ex.Message}");
+                Console.Error.WriteLine(ex.StackTrace);
+            }
         }
     }
 }
